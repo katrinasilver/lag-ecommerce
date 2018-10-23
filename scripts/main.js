9ba0aae2b0eb8883ac21fff27b6ca3c60687f900
Katrina Agustin <katrina.agustin@gmail.com>
2018-10-23 14:36:34 -0700

    sign-up form va;lidation WIP

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,18 @@ const checkout = require('./partials/checkout')
 const homepage = require('./partials/homepage')
 const products = require('./partials/products')
 
+/* Homepage Scripts */
+
 $('#carousel').carousel() // initialize the homepage carousel
 
 let addCartButton = document.querySelectorAll('.btn.btn-primary')
 addCartButton.forEach(a => {
   a.addEventListener('click', (e) => {
     e.preventDefault() //hijack all btn-primaries behavior from scrolling to top when clicked!
+
+    if (a.classList.contains('signup') && document.querySelector('#email').value !== '') {
+      document.querySelector('.alert.alert-success').classList.remove('d-none') // add setInterval later
+    }
   })
 })
 
