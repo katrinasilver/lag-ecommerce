9e7d8d8b78a6de1000d95a1d27e2017db33f7f24
Katrina Agustin <katrina.agustin@gmail.com>
2018-11-08 16:16:49 -0800

    put checkout js back

---
+++ scripts/partials/checkout.js
@@ -0,0 +1,47 @@
+var shippingFirstName = document.querySelector('#shipping-firstname')
+var shippingLastName = document.querySelector('#shipping-lastname')
+var shippingCompany = document.querySelector('#shipping-companyName')
+var shippingAddress = document.querySelector('#shipping-address')
+var shippingAppart = document.querySelector('#shipping-appar')
+var shippingTown = document.querySelector('#shipping-town')
+var shippingState = document.querySelector('#shipping-state')
+var shippingZipcode = document.querySelector('#shipping-zipcode')
+
+var billingFirstName = document.querySelector('#billing-firstname')
+var billingLastName = document.querySelector('#billing-lastname')
+var billingCompanyName = document.querySelector('#billing-companyName')
+var billingAdress = document.querySelector('#billing-address')
+var billingAppart = document.querySelector('#billing-appar')
+var billingTown = document.querySelector('#billing-town')
+var billingState = document.querySelector('#billing-state')
+var billingzipcode = document.querySelector('#billing-zipcode')
+
+var allBidds = [billingFirstName, billingLastName, billingCompanyName, billingAdress, billingAppart, billingTown, billingState, billingzipcode]
+
+var checkBox = document.querySelector('#checkbox-validation');
+console.log(checkBox);
+
+checkBox.addEventListener('change', function (e) {
+  if (checkBox.checked) {
+    checker = true;
+    billingFirstName.value = shippingFirstName.value;
+    billingLastName.value = shippingLastName.value;
+    billingCompanyName.value = shippingCompany.value;
+    billingAdress.value = shippingAddress.value;
+    billingAppart.value = shippingAppart.value;
+    billingTown.value = shippingTown.value;
+    billingState.value = shippingState.value;
+    billingzipcode.value = shippingZipcode.value;
+  } else {
+    allBidds.forEach(element => {
+      element.value = '';
+      checker = true;
+    });
+  }
+})
+
+document.querySelector(".checkout-box").addEventListener('submit', function (e) {
+  alert("Thank you for ordering our Dinos!")
+})
+
+module.exports = {}
